d2576fb2f3bdc03f11a3c121b762543d5a88cb9f
Harsha9291 <harshharsh815000@gmail.com>
2025-03-12 07:08:18 +0530

    python code

---
+++ Desktop/js/first.py
@@ -0,0 +1,28 @@
+'''print("harshjndc")
+print("harshjndc")
+
+print("harshjndcrererre")
+
+print("harshjndcreferfer")'''
+
+'''name = "Rahul"
+age = 25
+height = 5.8
+is_student = True
+
+print(name, age, height, is_student)
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")'''
+
+#odd number
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+

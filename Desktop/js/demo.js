843e5b2a0fecd41def0e191b8b0a817329cc6f9a
Harsha9291 <harshharsh815000@gmail.com>
2025-03-12 07:22:11 +0530

    python code

---
+++ Desktop/js/demo.js
@@ -0,0 +1,77 @@
+/*const student = {
+    fullname: "harsh gond",
+    age: 21,
+    ispaased :true,
+    percentage : 51,
+  
+};
+student ["age"] =student["age"] + 1;
+student["fullname"] = "harshit pandus";
+console. log(student.fullname);*/
+
+/*let a = 10;
+let b = 2;
+console. log("a+b =", a + b);
+console. log("a-b =",a - b);
+console. log("a*b =",a * b);
+console. log("a/b =",b / a);
+console. log("a%b =",a % b);
+console. log("a**b =",a ** b);*/
+//unary operater
+/*let a = 10;
+let b = 2;
+console. log("a =",a, "and b",b);
+a = a++;
+console. log("a=",a);
+console. log("a=",a);*/
+
+//assignment operators
+/*let a =5;
+let b = 2; 
+a += 4// 4 is add on a =9
+
+ console. log("a =",a);*/
+
+ //comparison operators
+ /*let a = 5;
+ let b= 5;
+ console.log("5==2", a==b);
+
+ let a = 5;
+ let b= 2;
+ console.log("5!=2", a!=b);
+ 
+ let a = 5;
+ let b= "5";
+ console.log("5===2", a===b);
+
+  
+ let a = 5;
+ let b= "5";
+ console.log("5!==2", a!==b);
+
+ let a = 5;
+ let b= 2;
+ console.log("5>=2", a>=b);
+
+
+ //logical operator
+
+let a = 8;
+let b = 6;
+let cond1 = a > b; 
+let cond2 = b ===6;
+console.log("cond1 && cond2 =",cond1 && cond2);  //all condtion must true then its provide true otherwise provide flase
+
+
+let a = 8;
+let b = 7;
+let cond1 = a > b; 
+let cond2 = b ===6;
+console.log("cond1 || cond2 =",cond1 || cond2);  //if any  condtion is true then its provide true 
+*/
+let a = 1;
+let b = 7;
+let cond1 = a > b; 
+let cond2 = b ===8;
+console.log("!(a<b)=",!(a<b));
